683b6eb30b5471f798be78f8b4b1cb02b832337b
Luana Barros <98184881+luabarross@users.noreply.github.com>
2022-06-19 21:00:59 +0100

    Create esquadro.js

---
+++ esquadro.js
@@ -0,0 +1,16 @@
+var tela = document.querySelector("canvas");
+var pincel = tela.getContext("2d");
+
+pincel.fillStyle = "black";
+pincel.beginPath();
+pincel.moveTo(50, 50);
+pincel.lineTo(50, 400);
+pincel.lineTo(400, 400);
+pincel.fill();
+
+pincel.fillStyle = "red";
+pincel.beginPath();
+pincel.moveTo(100, 175);
+pincel.lineTo(100, 350);
+pincel.lineTo(275, 350);
+pincel.fill();
